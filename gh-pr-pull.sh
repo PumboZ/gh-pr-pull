8d72699d2f336df0740b62deef320e5ac7122342
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 <andre.4drk@gmail.com>
2022-02-21 10:31:54 +0100

    clean useless stuff and add comments for clarity

--- gh-pr-pull.sh
+++ gh-pr-pull.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-# sudo apt-get install jq
-# e anche gh!
+# install jq and gh
 
+# insert your own base path
 base_path="$HOME/Projects"
 declare -A messages
 bot_token="xoxb-3129647379621-3156303839808-xqVKWGaB41XKGa3IEGsPU0hT"
 
 
 execute_user(){
-	echo $1
-	echo $2
-	echo $3
 	IFS=',' read -ra prjs <<< "$3"
-	# $3 dovrebbe essere un array, adatta a ciclo!
 	for prj in ${prjs[@]}; do
 		cd "$base_path/$prj"
 		r=$(gh pr list --json url)
@@ -23,15 +19,12 @@ execute_user(){
 			message+="-$url
 		"
 		done
-		echo $message
 		$(curl -X POST "https://slack.com/api/chat.postMessage" -H "accept: application/json" -d token=$bot_token -d channel=$2 -d text="$message" -d as_user=true)
 	done
 }
 
+# $1 username
+# $2 user slack_id
+# $3 projects comma separated string
 execute_user andrea U033WGGV459 leia,c-3po,lando-v2
-execute_user alex U033WGGV459 leia,c-3po,lando-v2
-#for str in ${projects[@]}; do
 
-#done
-
-#$(curl -X POST "https://slack.com/api/chat.postMessage" -H "accept: application/json" -d token=$bot_token -d channel=$number_id -d text="Hello" -d as_user=true)
