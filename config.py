041bdb0a222284314e8b7f705a7be8641644d292
Andrea Palumbo <andre.4drk@gmail.com>
2022-05-15 13:19:36 +0200

    adding user config file

---
+++ config.py
@@ -0,0 +1,14 @@
+users_info = [
+	{
+		'gh':'PumboZ',
+		'slack':'U033WGGV459',
+		'projects':{
+						'leia':{
+									'filters':['is:pr','is:open','assignee:PumboZ'],
+								},
+						'c-3po':{
+									'filters':['is:pr','is:open','assignee:PumboZ'],
+								},
+					}
+	},
+]
