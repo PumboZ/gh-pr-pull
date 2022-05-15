c37aeb12690b372c9f2c1526df01ea9ffc32d339
Andrea Palumbo <andre.4drk@gmail.com>
2022-05-15 13:19:00 +0200

    prep for config.py and better messages

--- gh-pr-pull.py
+++ gh-pr-pull.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from config import users_info
 
 message = ""
 start_slack_message_block = '[{"type":"header","text": {"type":"plain_text","text":"%s"}},'
@@ -9,9 +10,6 @@ project_slack_message_block = '{"type":"section","text":{"type":"mrkdwn","text":
 git_api_path = "https://api.github.com/"
 slack_api_send_message = "https://slack.com/api/chat.postMessage"
 
-users_info = [
-{'gh':'git_user','slack':'slack_member_ID'},
-]
 prs = {}
 
 slack_bot_token = "xoxb-...."
@@ -20,18 +18,20 @@ github_requests = requests.Session()
 github_requests.auth = ('git_user','git_access_token')
 slack_requests = requests.Session()
 
-# prendi le pr delle repo per utente
+def getFilters(repo_filters, repo_name):
+	return '%20'.join(repo_filters) + "%20repo:everli/" + repo_name
+
 for user in users_info:
-	json_repos = github_requests.get(git_api_path + "users/" + user['gh'] + "/subscriptions").json()
 	message = start_slack_message_block % ("Hello " + user['gh'] + "! \n Here's the list of open PRs to check! ")
-	for repo in json_repos:
-		if(repo['owner']['login'] == "everli"):
-			if (not (repo['name'] in prs)):
-				json_prs = github_requests.get(git_api_path + "repos/everli/" + repo['name'] + "/pulls?state=open").json()
-				prs[repo['name']] = "*List of pending PRs on <https://github.com/everli/" + repo['name'] + "/pulls|" + repo['name'] + "> project:* \n"
-				for pr in json_prs:
-					prs[repo['name']] += "*<" + pr['html_url'] + "|#" + str(pr['number']) + " - " + pr['title'] + ">* \n"
-			message += divider_slack_message_block + project_slack_message_block % (prs[repo['name']])
+	for repo in user['projects']:
+		filters = getFilters(user['projects'][repo]['filters'], repo)
+		json_prs = github_requests.get(git_api_path + "search/issues?q=" + filters).json()
+		prs = "*List of pending PRs on <https://github.com/everli/" + repo + "/pulls|" + repo + "> project:* \n"
+		prs += "_{filter used: " + requests.utils.unquote(filters) + "}_\n"
+		for pr in json_prs['items']:
+			prs += "*<" + pr['html_url'] + "|#" + str(pr['number']) + " - " + pr['title'] + ">* \n"
+		message += divider_slack_message_block + project_slack_message_block % (prs)
+	
 	#curl slack
 	slack_data={'token':slack_bot_token,
 				'channel':user['slack'],
@@ -39,3 +39,4 @@ for user in users_info:
 				'blocks': message + "]",
 				'as_user': True}
 	slack_requests.post(url = slack_api_send_message, data = slack_data)
+	
